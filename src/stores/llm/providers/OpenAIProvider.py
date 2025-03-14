e5c21fa85946f09657b964688ceea2afd4893b98
EsraaMadi <esraa.sabry.mohamed@gmail.com>
2025-03-14 21:52:27 +0300

    fix openai providor

--- src/stores/llm/providers/OpenAIProvider.py
+++ src/stores/llm/providers/OpenAIProvider.py
@@ -26,7 +26,7 @@ class OpenAIProvider(LLMInterface):
 
         self.client = OpenAI(
             api_key = self.api_key,
-            #api_url = self.api_url
+            base_url = self.api_url if self.api_url and len(self.api_url) else None
         )
 
         self.enums = OpenAIEnums
